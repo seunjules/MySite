ef72ed6ad8ff891f8e9864e539f9ad6e507f156f
Seunjules <seun.mejule@gmail.com>
2019-09-26 00:20:02 -0600

    fixing CSS bugs

--- src/constants.js
+++ src/constants.js
@@ -7,10 +7,10 @@ export default {
                 dates. CultivatR aims to reduce wastes in the produce and groceries indusrty, by changing the way we buy food`,
   cultivatrTech: `The Front End of the App is built using the React.js library. The server is written in Python,
                 using the Flask micro web framework. We used SQLAlchemy Object Relational Mapper. The database
-                 is a Postgres database`,
+                 is Postgres SQL`,
 
   julesMarketBlurb: `Jules market is an online clothing store. Users browse items by category
-                , or by gender. Items can be added and removed from a shopping cart. The App is integrated with the Stripe API
+                , or by gender. Items can be added to, and removed from, a shopping cart. The App is integrated with the Stripe API
                 to simulate payments`,
   julesMarketTech: `Jules Market is built with React.js. styling is done with SCSS. It uses Redux for state management. The App is integrated wtith Google Firebase.
                 Firebase is used for handling user authenthication and session management. It is also used to store the Apps data`,
